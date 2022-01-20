6eb0f513caf4db59c0dfe2181e5bb0f7924e89a7
Ioannis Michalopoulos <46590599+imichalop@users.noreply.github.com>
2022-01-20 13:26:54 +0200

    Update Uncompress_and_Convert.php

--- Parsers/Uncompress_and_Convert.php
+++ Parsers/Uncompress_and_Convert.php
@@ -1,5 +1,5 @@
 <?php
-//Usage: php Uncompress_and_Covnert.php <Series main directory>
+//Usage: php Uncompress_and_Convert.php <Series main directory>
 $main_directory = rtrim($argv[1],"/"); //trim directory input
 
 if(is_dir($main_directory) && $argc==2){
@@ -8,9 +8,25 @@ if(is_dir($main_directory) && $argc==2){
   for($y=2;$y<$dirnum;$y++){
     $dirpath = "$main_directory"."/"."$dir[$y]";
     echo "$dirpath\n";
-    exec("unzip $dirpath/*.zip");
-    exec("gunzip $dirpath/*.gz");
+    exec("unzip $dirpath/*.zip 2>&1", $retArr, $retVal);
+    exec("gunzip $dirpath/*.gz 2>&1", $retArr, $retVal);
+    exec("tar -xvf $dirpath/*.tar 2>&1", $retArr, $retVal);
     exec("apt-cel-convert -f text -i $dirpath/*.CEL");
+  }
+  for($y=2;$y<$dirnum;$y++){
+    $flag = 0;
+    $dirpath = "$main_directory"."/"."$dir[$y]";
+    $files = scandir($dirpath);
+    $filenum = count($files);
+    for($i=0;$i<$filenum;$i++){
+        $rest = substr($files[$i], -4);
+        if($rest ==".cel" || $rest== ".CEL"){
+            $flag=1;
+        }
+    }
+    if($flag==0){
+        exec("rm -rf $dirpath");
+    }
   }
 }
 else{
